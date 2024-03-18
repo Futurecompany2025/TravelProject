9a91e75cb7307fdeee2e3062fe335e2c89ea4bcb
Dipeshlohani <49984957+shikhardahal@users.noreply.github.com>
2024-03-18 09:54:40 +0545

    css changes

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import logo from '../assets/img/logo/logo.png';
-import HeroSlider from './HeroSlider'; // Import the HeroSlider component
-
+import HeroSlider from './HeroSlider';
+ // Import the HeroSlider component
 const Header = () => {
     return (
         <header>
-            {/* Your header content */}
             <div className="header-area">
                 <div className="main-header">
                     <div className="header-top top-bg d-none d-lg-block">
@@ -36,38 +35,41 @@ const Header = () => {
                     <div className="header-bottom  header-sticky">
                         <div className="container">
                             <div className="row align-items-center">
-                                {/* Logo */}
                                 <div className="col-xl-2 col-lg-2 col-md-1">
                                     <div className="logo">
-                                        <a href="index.html"><img src={logo} alt="" style={{ width: '150px', height:'50px'}} /></a>
+                                        <a href="index.html"><img src={logo} alt="Logo" style={{ width: '300px', height:'100px'}} /></a>
                                     </div>
                                 </div>
                                 <div className="col-xl-10 col-lg-10 col-md-10">
-                                    {/* Main-menu */}
-                                    <div className="main-menu f-right d-none d-lg-block" style={{ width: '100%', height: '90px' }}> 
-                                        <nav>               
-                                            <ul id="navigation">
-                                                <li>
-                                                    <a href="#">Destinations</a>
-                                                    <ul className="submenu">
-                                                        <li><a href="#">Nepal</a></li>
-                                                        <li><a href="#">Bhutan</a></li>
-                                                        <li><a href="#">Tibet</a></li>
-                                                        <li><a href="#">India</a></li>
-                                                    </ul>
-                                                </li>
-                                                <li><a href="about.html">Day Tours</a></li>
-                                                <li><a href="packages.html">Luxury Travel</a></li>
-                                                <li><a href="blog.html">Travel Style</a></li>
-                                                <li><a href="blog.html">Travel Guide</a></li>
-                                                <li><a href="blog.html">Blogs</a></li>
-                                                <li><a href="blog.html">Company</a></li>
-                                                <li><a href="contact.html">Contact Us</a></li>
-                                            </ul>
-                                        </nav>
-                                    </div>
+                                    <nav className="main-menu f-right d-none d-lg-block">               
+                                        <ul id="navigation">
+                                            <li className="menu-item-has-children">
+                                                <a href="#">Destinations</a>
+                                                <ul className="submenu">
+                                                    <li><a href="#">Nepal</a>
+                                                        <ul>
+                                                            <li><a href="#">Trekking in Nepal</a></li>
+                                                            <li><a href="#">Tours in Nepal</a></li>
+                                                            <li><a href="#">Luxury Tours in Nepal</a></li>
+                                                            <li><a href="#">Peak Climbing in Nepal</a></li>
+                                                            <li><a href="#">Expeditions in Nepal</a></li>
+                                                        </ul>
+                                                    </li>
+                                                    <li><a href="#">India</a></li>
+                                                    <li><a href="#">Bhutan</a></li>
+                                                    <li><a href="#">Tibet</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="about.html">Day Tours</a></li>
+                                            <li><a href="packages.html">Luxury Travel</a></li>
+                                            <li><a href="blog.html">Travel Style</a></li>
+                                            <li><a href="blog.html">Travel Guide</a></li>
+                                            <li><a href="blog.html">Blogs</a></li>
+                                            <li><a href="blog.html">Company</a></li>
+                                            <li><a href="contact.html">Contact Us</a></li>
+                                        </ul>
+                                    </nav>
                                 </div>
-                                {/* Mobile Menu */}
                                 <div className="col-12">
                                     <div className="mobile_menu d-block d-lg-none"></div>
                                 </div>
@@ -76,9 +78,7 @@ const Header = () => {
                     </div>
                 </div>
             </div>
-            {/* Header End */}
         </header>
     );
 };
-
 export default Header;
